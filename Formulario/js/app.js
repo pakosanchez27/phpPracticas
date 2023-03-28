9fb9e1d96af716febea104eca505c950ddc89e80
Francisco Sánchez <javier.sanchezjps27@gmail.com>
2023-03-28 05:01:39 -0600

    UPDATE: actualizando proyecto

--- Formulario/js/app.js
+++ Formulario/js/app.js
@@ -8,15 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputNombre = document.querySelector('#nombre');
     const inputApellido = document.querySelector('#apellido');
     const inputEmail = document.querySelector('#email');
-    const inputPass = document.querySelector('#contraseña');
-
+    const inputPass = document.querySelector('#password');
+    const btncrear = document.querySelector('#btnCrear');
+    const btnlimpiar = document.querySelector('#limpiar');
+    const formulario = document.querySelector('#formulario');
+    
     const registro = {
         nombre: '',
         apellido: '',
         email: '',
-        contraseña: ''
+        password: ''
     }
-
+   
 
     //Eventos 
 
@@ -24,6 +27,20 @@ document.addEventListener('DOMContentLoaded', () => {
     inputApellido.addEventListener('blur', validar);
     inputEmail.addEventListener('blur', validar);
     inputPass.addEventListener('blur', validar);
+    btncrear.addEventListener('click', function (e) {
+        e.preventDefault();
+        console.log('Hola mundo');
+    });
+    btnlimpiar.addEventListener('click', function (e) {
+        e.preventDefault();
+        registro.nombre = '';
+        registro.apellido = '';
+        registro.email = '';
+        registro.password = '';
+
+        formulario.reset();
+        verificar();
+    })
 
     // Funciones
 
@@ -33,6 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target.value.trim() === '') {
             campo.classList.add('alerta');
             registro[e.target.name] = '';
+            verificar();
+            
         }else{
             campo.classList.remove('alerta');
         }
@@ -41,5 +60,17 @@ document.addEventListener('DOMContentLoaded', () => {
         registro[e.target.name] = e.target.value.trim().toLowerCase();
         console.log(registro);
     };
+
+    function verificar() {
+        console.log(Object.values(registro));
+
+        if (Object.values(registro).includes('')) {
+            btncrear.classList.add('opacity-50');
+            btncrear.disabled = true;
+        }else{
+            btncrear.classList.remove('opacity-50');
+            btncrear.disabled = false;
+        }
+    }
 
 });
